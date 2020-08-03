3f48e776454dee298a6c7410e4d29f5ffa062730
tsujimotp67 <kotaro6795@gmail.com>
2020-08-03 19:43:06 +0900

    パスワード項目追加

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -21,17 +21,25 @@
                             <form method="POST" action="{{route('login')}}">
                                 @csrf
                                 <div class="md-form">
-                                    <label for="email">
-                                        メールアドレス
-                                    </label>
+                                    <label for="email">メールアドレス</label>
                                     <input
                                         class="form-control"
                                         type ="text"
                                         id="email"
                                         name="email"
-                                        required value="{{old('email')}}"
+                                        required
+                                        value="{{old('email')}}"
                                     >
                                 </div>
+                                <div class="md-form">
+                                    <label for="password">パスワード</label>
+                                    <input 
+                                        class="form-control"
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        required
+                                    >
                                 <input type="hidden" name="remember" id="remember" value="on">
                                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
                             </form>
